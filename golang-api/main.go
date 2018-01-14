d102500341c892a0d40514e6c55dfd965aa87823
yogesh.s <yogesh.s@aux.go-jek.com>
2018-01-14 15:56:50 +0530

    fixing default run to be startES

--- golang-api/main.go
+++ golang-api/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	serverMode = kingpin.Command("startES", "Starting Elastic based search api")
+	serverMode = kingpin.Command("startES", "Starting Elastic based search api").Default()
 	indexCreationMode = kingpin.Command("startPG", "Starting Postgres free text based search api")
 )
 
@@ -19,10 +19,13 @@ func main() {
 	// cmd line mode will have precedence over env defined
 
 	cmdLineMode := kingpin.Parse()
-	mode := ""
-	if cmdLineMode == "" {
-		mode = os.Getenv("mode")
+	envMode := os.Getenv("mode")
+	mode := envMode
+	if envMode == "" {
+		mode = cmdLineMode
 	}
+
+	log.Printf("starting mode: %s", mode)
 
 	switch mode {
 		case "startPG":
